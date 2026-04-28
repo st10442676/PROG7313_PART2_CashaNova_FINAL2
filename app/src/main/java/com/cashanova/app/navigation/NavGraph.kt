052bf212a9e3438d3cfb951f5625e93534f088c2
st10442676 <tiashapillay0407@gmail.com>
2026-04-28 13:22:04 +0200

    COMMIT 3 - (TIASHA BRANCH)(ST10442676)
    In this commit I created the NavGraph.kt file and added navigation route constants

---
+++ app/src/main/java/com/cashanova/app/navigation/NavGraph.kt
@@ -0,0 +1,10 @@
+package com.cashanova.app.navigation
+
+object Routes {
+    const val LOGIN = "login"
+    const val REGISTER = "register"
+    const val DASHBOARD = "dashboard"
+    const val EXPENSES = "expenses"
+    const val SEARCH = "search"
+}
+
